d834afa2719492f0d78651ac3c32fe389ff008c1
Eugenio-Iglesias <eugene.iglesias@gmail.com>
2021-06-04 15:10:41 -0700

    basic homepage menu layoutand styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "./App.css";
-import "./homepage.component";
-import Homepage from "./homepage.component";
+import "./pages/homepage/homepage.component";
+import Homepage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
-    (
     <div>
       <Homepage />
-      </div>
- 
-  ));
+    </div>
+  );
 }
 
 export default App;
