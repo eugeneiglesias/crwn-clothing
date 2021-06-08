d3833460d5c4b7e467079709eb19e0608d47f3a1
Eugenio-Iglesias <eugene.iglesias@gmail.com>
2021-06-08 12:14:48 -0700

    updating app with collection preview comp an d collection item component

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -1,35 +1,22 @@
 import React from "react";
+
+import CollectionItem from "../collection-item/collection-item.component";
+
 import "./collection-preview.style.scss";
 
 const CollectionPreview = ({ title, routeName, items }) => (
   <div className="collection-preview">
-    <h1 className="title"><a href={routeName}>{title.toUpperCase()}</a></h1>
+    <h1 className="title">
+      <a href={routeName}>{title.toUpperCase()}</a>
+    </h1>
     <div className="preview">
-      {items.map((item) => (
-        <div key={item.id} className="item" >
-
-
-
-        <div
-          className="background-image"
-          style={{
-            backgroundImage: `url(${item.imageUrl})`,
-          }}
-        />
-        <div className="content">
-          <h1 className="title">{item.name.toUpperCase()}</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-
-
-
-
-
-
-
-
-        </div>
-      ))}
+      {items
+        //filter by item and index
+        //all these could have perfomance issues if the array gets too large
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
   </div>
 );
